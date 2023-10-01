ae7891845d97470d313c52983c470a8b078c5073
Hélio Marques <helio111171souza@gmail.com>
2023-10-01 13:07:20 -0300

    Início MVC Client POO Visual

---
+++ Projeto/ClientDSWK/View/UGridBase.pas
@@ -0,0 +1,56 @@
+unit UGridBase;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Grids, Vcl.DBGrids, Vcl.StdCtrls,
+  Vcl.Buttons, Vcl.ExtCtrls, Data.DB;
+
+type
+  TFGridBase = class(TForm)
+    PHead: TPanel;
+    PRodape: TPanel;
+    PView: TPanel;
+    BBIncluir: TBitBtn;
+    BBAlterar: TBitBtn;
+    BBExcluir: TBitBtn;
+    BBAtualizar: TBitBtn;
+    BBSair: TBitBtn;
+    LNR: TLabel;
+    ENR: TEdit;
+    DBGView: TDBGrid;
+    DSBase: TDataSource;
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure BBSairClick(Sender: TObject);
+    procedure BBAtualizarClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FGridBase: TFGridBase;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFGridBase.BBAtualizarClick(Sender: TObject);
+begin
+  DSBase.DataSet.Refresh;
+end;
+
+procedure TFGridBase.BBSairClick(Sender: TObject);
+begin
+  DSBase.DataSet.Close;
+  Close;
+end;
+
+procedure TFGridBase.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  Action := CaFree;
+end;
+
+end.
